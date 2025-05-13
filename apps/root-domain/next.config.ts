63668fc38be61616e64fe8c4a82ca36de4ab0d7b
Nav Ebrahimi <nav@clerk.dev>
2025-05-13 16:48:41 -0400

    update next.config.mjs to .ts

---
+++ apps/root-domain/next.config.ts
@@ -0,0 +1,12 @@
+// import { nextJsConfig } from '@repo/eslint-config/next-js';
+
+// /** @type {import("eslint").Linter.Config} */
+// export default nextJsConfig;
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
