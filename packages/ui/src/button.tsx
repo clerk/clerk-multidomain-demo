3582c4fd8f662763b40ae8b917cd7060a93f5b74
Roy Anger <roy@royanger.com>
2025-05-24 23:21:20 -0400

    chore: Moved button and card to @repo/ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-purple-600",
+        default: "bg-black text-primary-foreground hover:bg-purple-600 hover:text-white",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
