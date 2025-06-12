c09a66af96f08a4bcd052c3bcf12a8c0d2b30251
Nav Ebrahimi <nav@clerk.dev>
2025-06-12 19:22:46 -0400

    added t3-env & zod

---
+++ apps/next.js/root-domain/src/env.ts
@@ -0,0 +1,21 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod/v4';
+
+export const env = createEnv({
+  server: {
+    CLERK_SECRET_KEY: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_ALLOWED_REDIRECT_ORIGINS: z.string(),
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(),
+  },
+  runtimeEnv: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_ALLOWED_REDIRECT_ORIGINS:
+      process.env.NEXT_PUBLIC_ALLOWED_REDIRECT_ORIGINS,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+  },
+});
