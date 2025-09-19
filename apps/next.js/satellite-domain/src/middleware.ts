fe077b52bde24a3457c5b08bcb195a0019115060
guilherme6191 <guilherme@clerk.dev>
2025-09-19 17:48:04 -0300

    fix: Add workaround to avoid protect() to trigger redirect loop

--- apps/next.js/satellite-domain/src/middleware.ts
+++ apps/next.js/satellite-domain/src/middleware.ts
@@ -1,9 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    await auth.protect();
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.redirect(
+        new URL(
+          `/sign-in?redirect_url=${encodeURIComponent(request.url)}`,
+          process.env.NEXT_PUBLIC_ROOT_DOMAIN_URL || "http://localhost:3000"
+        )
+      );
+    }
+    return NextResponse.next();
   }
 });
 
