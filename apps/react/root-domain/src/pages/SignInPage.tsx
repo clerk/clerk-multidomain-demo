8aa9a83a56197de65a3685b9612a3d2c38690b11
guilherme6191 <guilherme@clerk.dev>
2025-09-26 09:56:19 -0300

    fix: Remove routing path to fix flickering

--- apps/react/root-domain/src/pages/SignInPage.tsx
+++ apps/react/root-domain/src/pages/SignInPage.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 export default function SignInPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <SignIn routing="path" path="/sign-in" />
+      <SignIn />
     </div>
   );
 }
